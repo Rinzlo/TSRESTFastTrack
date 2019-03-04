e5397d38e01795e5a61a2024b98459333b23338e
Rinzlo <joshuarco@gmail.com>
2019-03-03 22:43:33 -0800

    Basics

--- build/server.js
+++ build/server.js
@@ -5,12 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = express_1.default();
+const apiGetTours_1 = require("./api/tours/apiGetTours");
+const apiGetTourDetail_1 = require("./api/tours/apiGetTourDetail");
 app.get("/", (req, res, next) => {
     res.send("Tour Booking API");
 });
-app.get("/tours", (req, res, next) => {
-    res.send("Get a list of tours...");
-});
+app.get("/tours", apiGetTours_1.apiGetTours);
+app.get("/tours/:id", apiGetTourDetail_1.apiGetTourDetail);
 app.post("/tours", (req, res, next) => {
     res.send("Add a new tour...");
 });
